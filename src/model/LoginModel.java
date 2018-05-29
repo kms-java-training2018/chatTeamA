af81c688de6ddc8334a528c0df44f3c01fa7fc16
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-05-29 17:08:47 +0900

    データのプル

--- src/model/LoginModel.java
+++ src/model/LoginModel.java
@@ -13,61 +13,61 @@ import bean.LoginBean;
  */
 public class LoginModel {
 
-	public LoginBean authentication(LoginBean bean) {
-		// 初期化
-		StringBuilder sb = new StringBuilder();
-		String userId = bean.getUserId();
-		String password = bean.getPassword();
+    public LoginBean authentication(LoginBean bean) {
+        // 初期化
+        StringBuilder sb = new StringBuilder();
+        String userId = bean.getUserId();
+        String password = bean.getPassword();
 
-		Connection conn = null;
-		String url = "jdbc:oracle:thin:@192.168.51.67:1521:XE";
-		String user = "DEV_TEAM_A";
-		String dbPassword = "A_DEV_TEAM";
-		// JDBCドライバーのロード
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch (ClassNotFoundException e) {
-			bean.setErrorMessage("データベースと接続出来ませんでした");
-			e.printStackTrace();
-		}
-		// 接続作成
-		try {
-			conn = DriverManager.getConnection(url, user, dbPassword);
+        Connection conn = null;
+        String url = "jdbc:oracle:thin:@192.168.51.67:1521:XE";
+        String user = "DEV_TEAM_A";
+        String dbPassword = "A_DEV_TEAM";
+        // JDBCドライバーのロード
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            bean.setErrorMessage("データベースと接続出来ませんでした");
+            e.printStackTrace();
+        }
+        // 接続作成
+        try {
+            conn = DriverManager.getConnection(url, user, dbPassword);
 
-			// SQL作成
-			sb.append("SELECT ");
-			sb.append(" user_no ");
-			sb.append(" ,user_name ");
-			sb.append("FROM ");
-			sb.append(" m_user ");
-			sb.append("WHERE ");
-			sb.append(" user_id = '" + userId + "' ");
-			sb.append(" AND password = '" + password + "'");
+            // SQL作成
+            sb.append("SELECT ");
+            sb.append(" user_no ");
+            sb.append(" ,user_name ");
+            sb.append("FROM ");
+            sb.append(" m_user ");
+            sb.append("WHERE ");
+            sb.append(" user_id = '" + userId + "' ");
+            sb.append(" AND password = '" + password + "'");
 
-			// SQL実行
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sb.toString());
+            // SQL実行
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sb.toString());
 
-			if (!rs.next()) {
-				bean.setErrorMessage("パスワードが一致しませんでした。");
-			} else {
-				bean.setUserNo(rs.getString("user_no"));
-				bean.setUserName(rs.getString("user_name"));
-				bean.setErrorMessage("");
-				conn.close();
+            if (!rs.next()) {
+                bean.setErrorMessage("会員IDもしくはパスワードが間違っています。");
+            } else {
+                bean.setUserNo(rs.getString("user_no"));
+                bean.setUserName(rs.getString("user_name"));
+                bean.setErrorMessage("");
+                conn.close();
 
-			}
-		} catch (SQLException e) {
-			bean.setErrorMessage("データがありませんでした。");
-			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+            }
+        } catch (SQLException e) {
+            bean.setErrorMessage("データがありませんでした。");
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
-		return bean;
-	}
+        return bean;
+    }
 }
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,51 +19,63 @@ import model.LoginModel;
  */
 public class LoginServlet extends HttpServlet {
 
-	/**
-	 * 初期表示
-	 */
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// Beanの初期化
-		LoginBean bean = new LoginBean();
-		bean.setErrorMessage("");
-		bean.setUserId("");
-		bean.setPassword("");
+    /**
+     * 初期表示
+     */
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // Beanの初期化
+        LoginBean bean = new LoginBean();
+        bean.setErrorMessage("");
+        bean.setUserId("");
+        bean.setPassword("");
 
-		req.setAttribute("loginBean", bean);
-		req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, res);
-	}
+        req.setAttribute("loginBean", bean);
+        req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, res);
+    }
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// 初期化
-		LoginBean bean = new LoginBean();
-		LoginModel model = new LoginModel();
-		String direction = "/WEB-INF/jsp/login.jsp";
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // 初期化
+        LoginBean bean = new LoginBean();
+        LoginModel model = new LoginModel();
+        String direction = "/WEB-INF/jsp/login.jsp";
 
-		// パラメータの取得
-		String userId = (String) req.getParameter("userId");
-		String password = (String) req.getParameter("password");
+        // パラメータの取得
+        String userId = (String) req.getParameter("userId");
+        String password = (String) req.getParameter("password");
+        Pattern p = Pattern.compile("^[0-9a-zA-Z]+$");
+        Matcher mUserId = p.matcher(userId);
 
-		bean.setUserId(userId);
-		bean.setPassword(password);
+        Matcher mPassword = p.matcher(password);
 
-		// 認証処理
-		try {
-			bean = model.authentication(bean);
-			req.setAttribute("loginBean", bean);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// 取得に成功した場合セッション情報をセット
-		if ("".equals(bean.getErrorMessage())){
-			SessionBean sessionBean = new SessionBean();
-			sessionBean.setUserName(bean.getUserName());
-			sessionBean.setUserNo(bean.getUserNo());
-			HttpSession session = req.getSession();
-			session.setAttribute("session", sessionBean);
+        // パラメータの判定
+        if (userId.length() > 20 || password.length() > 20 || !mUserId.find() || !mPassword.find()) {
+            bean.setErrorMessage("入力された値は正しくありません");
+        } else {
+            bean.setUserId(userId);
+            bean.setPassword(password);
 
-			// 行き先を次の画面に
-			direction = "/main";
-		}
-		req.getRequestDispatcher(direction).forward(req, res);
-	}
+            // 認証処理
+            try {
+                bean = model.authentication(bean);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        req.setAttribute("loginBean", bean);
+
+        // 取得に成功した場合セッション情報をセット
+        if ("".equals(bean.getErrorMessage())) {
+            SessionBean sessionBean = new SessionBean();
+            sessionBean.setUserName(bean.getUserName());
+            sessionBean.setUserNo(bean.getUserNo());
+            HttpSession session = req.getSession();
+            session.setAttribute("session", sessionBean);
+
+            // 行き先を次の画面に
+            direction = "/main";
+        }
+        req.getRequestDispatcher(direction).forward(req, res);
+    }
 }
