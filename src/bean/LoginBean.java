f46cb1e2757d921588db568f1033048b83680349
teranoshita-yuta <teranoshita-yuta@KMS2013-71.kms.local>
2018-05-30 10:20:32 +0900

    ローカルコミットテスト

--- src/bean/LoginBean.java
+++ src/bean/LoginBean.java
@@ -1,59 +1,59 @@
 package bean;
 
 public class LoginBean {
-	/** 会員ID */
-	private String userId;
+    /** 会員ID */
+    private String userId;
 
-	/** パスワード */
-	private String password;
+    /** パスワード */
+    private String password;
 
-	/** エラーメッセージ */
-	private String errorMessage;
+    /** エラーメッセージ */
+    private String errorMessage;
 
-	/** 表示名 */
-	private String userName;
+    /** 表示名 */
+    private String userName;
 
-	/** 会員番号 */
-	private String userNo;
+    /** 会員番号 */
+    private String userNo;
 
-	public String getUserId() {
-		return userId;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getErrorMessage() {
-		return errorMessage;
-	}
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getUserNo() {
-		return userNo;
-	}
+    public String getUserNo() {
+        return userNo;
+    }
 
-	public void setUserNo(String userNo) {
-		this.userNo = userNo;
-	}
+    public void setUserNo(String userNo) {
+        this.userNo = userNo;
+    }
 
 }
