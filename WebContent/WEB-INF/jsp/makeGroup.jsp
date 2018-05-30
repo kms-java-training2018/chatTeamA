4a118aee82d271cb1c32d10e74c67640cc97da16
okimoto-kei <okimoto-kei@KMS2013-83.kms.local>
2018-05-30 17:02:57 +0900

    5/30の変更部分

--- WebContent/WEB-INF/jsp/makeGroup.jsp
+++ WebContent/WEB-INF/jsp/makeGroup.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+  pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -8,14 +8,20 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>チャット研修プログラム</h1>
-	<h2>グループ作成</h2>
-	<form action="/chat/main" method="POST">
-		<input type="submit" value="グループを作成する">
-	</form>
+  <h1>チャット研修プログラム</h1>
+  <h2>グループ作成</h2>
 
-	<form action="/chat/main" method="POST">
-		<input type="submit" value="メインメニューに戻る">
-	</form>
+  <c:forEach var="obj" items="${user}">
+  <input type="checkbox" name="user" value="${obj}"> ${user}
+  </c:forEach>
+
+
+  <form action="/chat/main" method="POST">
+    <input type="submit" value="グループを作成する">
+  </form>
+
+  <form action="/chat/main" method="POST">
+    <input type="submit" value="メインメニューに戻る" >
+  </form>
 </body>
 </html>
--- src/servlet/MakeGroupServlet.java
+++ src/servlet/MakeGroupServlet.java
@@ -7,16 +7,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.MakeGroupBean;
+import model.MakeGroupModel;
+
 public class MakeGroupServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
-		req.getRequestDispatcher("/WEB-INF/jsp/makeGroup.jsp").forward(req, res);
-	}
+        MakeGroupBean bean = new MakeGroupBean();
+        bean.setErrorMessage("");
+        bean.setUserName("");
+        bean.setUserNo("");
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        req.setAttribute("makeGroupBean", bean);
+        req.getRequestDispatcher("/WEB-INF/jsp/makeGroup.jsp").forward(req, res);
+    }
 
-		req.getRequestDispatcher("/WEB-INF/jsp/makeGroup.jsp").forward(req, res);
-	}
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        MakeGroupBean bean = new MakeGroupBean();
+        MakeGroupModel model = new MakeGroupModel();
+
+
+
+
+
+
+
+
+
+        req.getRequestDispatcher("/WEB-INF/jsp/makeGroup.jsp").forward(req, res);
+    }
 
 }
