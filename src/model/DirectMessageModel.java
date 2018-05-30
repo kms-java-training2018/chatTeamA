7c3b2f3def789a61da59f15daf3af4b3cf600be0
noda-ayumi <noda-ayumi@KMS2013-56.kms.local>
2018-05-30 17:06:24 +0900

    5/30分

---
+++ src/model/DirectMessageModel.java
@@ -0,0 +1,111 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import bean.DirectMessageBean;
+
+/**
+ * メッセージ画面ビジネスロジック
+ */
+public class DirectMessageModel {
+    public DirectMessageBean authentication(DirectMessageBean bean) {
+        // 初期化
+        StringBuilder sb = new StringBuilder();
+        String userId = bean.getUserId();
+        String toSendUserNo = bean.getPassword();
+
+        Connection conn = null;
+        String url = "jdbc:oracle:thin:@192.168.51.67:1521:XE";
+        String user = "DEV_TEAM_A";
+        String dbPassword = "A_DEV_TEAM";
+        // JDBCドライバーのロード
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 接続作成
+        try {
+            conn = DriverManager.getConnection(url, user, dbPassword);
+
+            // SQL作成
+
+            //--会員番号と送信者番号、送信対象者番号を紐付けてメッセージ一覧取得--//
+            //--該当する送信者と送信対象者の一覧のみを表示--//
+            sb.append("SELECT");
+            sb.append("message");
+            sb.append("FROM");
+            sb.append("t_message_info A");
+            sb.append("inner join m_user B");
+            sb.append("on A.send_user_no = B.user_no");
+            sb.append("inner join m_user B");
+            sb.append("on A.to_send_user_no = B.user_no");
+            sb.append("WHERE ");
+            sb.append(" to_send_user_no = '" + toSendUserNo + "' ");
+            sb.append(" AND send_user_no = '" + userId + "'");
+
+            // SQL実行
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sb.toString());
+
+            ArrayList<String> list = new ArrayList<String>();
+
+            //--メッセージの分だけ取得--//
+            if (!rs.next()) {
+                bean.setErrorMessage("メッセージの取得に失敗しました");
+
+            } else {
+                while (rs.next()) {
+                bean.setMessage(rs.getString("message"));
+                list.add("message");
+                }
+                bean.setErrorMessage("");
+            }
+
+
+
+
+         // SQL作成
+
+            //--会員番号と送信者番号、送信対象者番号を紐付けてメッセージ一覧取得--//
+            //--該当する送信者と送信対象者の一覧のみを表示--//
+            sb.append("INSERT");
+            sb.append("message");
+            sb.append("FROM");
+            sb.append("t_message_info A");
+            sb.append("inner join m_user B");
+            sb.append("on A.send_user_no = B.user_no");
+            sb.append("inner join m_user B");
+            sb.append("on A.to_send_user_no = B.user_no");
+            sb.append("WHERE ");
+            sb.append(" to_send_user_no = '" + toSendUserNo + "' ");
+            sb.append(" AND send_user_no = '" + userId + "'");
+
+            // SQL実行
+            Statement stmt2 = conn.createStatement();
+            ResultSet rs2 = stmt.executeQuery(sb.toString());
+
+            ArrayList<String> list2 = new ArrayList<String>();
+
+
+            conn.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return bean;
+    }
+}
--- src/servlet/DirectMessageServlet.java
+++ src/servlet/DirectMessageServlet.java
@@ -7,16 +7,126 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.DirectMessageBean;
+import model.DirectMessageModel;
+
 public class DirectMessageServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
-		req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
-	}
+        req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
+    }
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
-		req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
+        // 初期化
+        DirectMessageBean bean = new DirectMessageBean();
+        DirectMessageModel model = new DirectMessageModel();
+        String direction = "/WEB-INF/jsp/directMessage.jsp";
 
-	}
+        /**
+         *
+         *パラメータチェック画面表示処理
+         *【処理概要】
+         *メインメニュー画面で対象ユーザーリンクを押したときに実行される処理。
+         *セッション情報チェック・会話情報取得処理が成功した場合、メッセージ画面に遷移する。
+         *
+         */
+
+        /*
+         * パラメータチェック
+         */
+
+        //--セッションの存在チェック--//
+
+        //存在しなければエラー画面へ遷移
+        //if (session = null) {
+        //direction = "/error";
+        //}
+
+        // パラメータの取得
+        String userId = (String) req.getParameter("userId");
+        String password = (String) req.getParameter("password");
+
+        bean.setUserId(userId);
+        bean.setPassword(password);
+
+        //--パラメータチェック--//
+
+        //送信対象者番号
+        String toSendUserNo = "";
+
+        //存在しなければエラー画面へ遷移
+        if (toSendUserNo.equals(null)) {
+            direction = "/error";
+        }
+
+        /*
+         * 会話情報取得処理
+         */
+
+        //--セッション情報の会員番号と、送信対象者の会員番号を条件に会話情報取得する処理--//
+        try {
+            bean = model.authentication(bean);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /**
+        *
+        *メッセージ送信処理
+        *【処理概要】
+        *メッセージ画面で送信ボタンを押したときに実行される処理。
+        *パラメータチェックに成功した場合、会話情報テーブルにメッセージを登録する。
+        *
+        */
+
+        /*
+         * パラメータチェック
+         */
+
+        //メッセージ画面で入力された情報を取得
+        String message = req.getParameter("name1");
+
+        //--パラメータチェック--//
+        //未入力の場合
+        if(message == null) {
+
+            //エラーメッセージ設定
+            bean.setErrorMessage("メッセージを入力してください");
+
+            //メッセージ画面へ遷移
+
+
+
+        //桁数チェック
+
+        } else if(message.length() > 1) {
+
+            //エラーメッセージ設定
+            bean.setErrorMessage("メッセージが長すぎます");
+
+            //メッセージ画面へ遷移
+
+
+        }
+
+        //--パラメータチェック完了--//
+
+
+
+        //        // 取得に成功した場合セッション情報をセット
+        //        if ("".equals(bean.getErrorMessage())) {
+        //            SessionBean sessionBean = new SessionBean();
+        //            sessionBean.setUserName(bean.getUserName());
+        //            sessionBean.setUserNo(bean.getUserNo());
+        //            HttpSession session = req.getSession();
+        //            session.setAttribute("session", sessionBean);
+        //
+        //            // 行き先を次の画面に
+        //            direction = "/directMessage";
+        //        }
+        req.getRequestDispatcher(direction).forward(req, res);
+        return;
+    }
 }
