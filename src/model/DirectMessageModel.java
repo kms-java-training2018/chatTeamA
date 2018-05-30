f0b5114522b8e200806eb6d3190d8ce39a5d9536
noda-ayumi <noda-ayumi@KMS2013-56.kms.local>
2018-05-30 17:06:46 +0900

    5/30分

--- src/model/DirectMessageModel.java
+++ src/model/DirectMessageModel.java
@@ -66,32 +66,7 @@ public class DirectMessageModel {
                 }
                 bean.setErrorMessage("");
             }
-
-
-
-
-         // SQL作成
-
-            //--会員番号と送信者番号、送信対象者番号を紐付けてメッセージ一覧取得--//
-            //--該当する送信者と送信対象者の一覧のみを表示--//
-            sb.append("INSERT");
-            sb.append("message");
-            sb.append("FROM");
-            sb.append("t_message_info A");
-            sb.append("inner join m_user B");
-            sb.append("on A.send_user_no = B.user_no");
-            sb.append("inner join m_user B");
-            sb.append("on A.to_send_user_no = B.user_no");
-            sb.append("WHERE ");
-            sb.append(" to_send_user_no = '" + toSendUserNo + "' ");
-            sb.append(" AND send_user_no = '" + userId + "'");
-
-            // SQL実行
-            Statement stmt2 = conn.createStatement();
-            ResultSet rs2 = stmt.executeQuery(sb.toString());
-
-            ArrayList<String> list2 = new ArrayList<String>();
-
+            
 
             conn.close();
 
