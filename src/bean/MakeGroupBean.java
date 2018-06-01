2e983b0a74818cced0b5c9650ba7982e93309c66
okimoto-kei <okimoto-kei@KMS2013-83.kms.local>
2018-06-01 11:53:14 +0900

    プル用コミット

--- src/bean/MakeGroupBean.java
+++ src/bean/MakeGroupBean.java
@@ -1,38 +1,87 @@
 package bean;
 
-public class MakeGroupBean {
-    /** 会員ID */
-    private String userId;
+import java.util.ArrayList;
 
-    /** パスワード */
-    private String password;
+public class MakeGroupBean {
+    /** 会員名 */
+    private ArrayList<String> userNameList = new ArrayList<String>();
+
+    /** 会員番号 */
+    private ArrayList<String> userNumList = new ArrayList<String>();
 
     /** エラーメッセージ */
     private String errorMessage;
 
-    /** 表示名 */
-    private String userName;
+    /**	グループ作成者のデータ */
+    private String autherUserName;
+    private String autherUserNo;
 
-    /** 会員番号 */
-    private String userNo;
 
-    /** グループ番号*/
-    private String groupNo;
 
-    public String getUserId() {
-        return userId;
+    //作成者のユーザー名を設定、ArrayListから検索し、そのindexからuserNameとuserNoを新しいListに追加
+    public void setAuther(String userName) {
+        this.autherUserName = userName;
+        int autherIndex = userNameList.indexOf(autherUserName);
+        //usernameが一応あるかでif
+        if (autherIndex >= 0) {
+
+            //作成者の名前から出したindexから、作成者のNoを全会員Noの値を検索してautherNoリストに設定
+            autherUserNo = userNumList.get(autherIndex);
+
+            //作成者のデータを全会員一覧から削除
+            userNameList.remove(autherIndex);
+            userNumList.remove(autherIndex);
+        }
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    //グループ名入力チェック
+        public int stringLengthCheck(String input) {
+            //返すメッセージを設定
+            int judgeByte =0;
+
+            // 何バイト分の長さであるかを取得
+            int length = input.getBytes().length;
+            // 最大バイト数の設定
+            int max = 30;
+
+            if ((int) length > max) { // 最大文字数よりも多かった場合
+                judgeByte = 1;
+                return judgeByte;
+            }
+            return judgeByte; // 許容内であった場合
+        }
+
+
+
+    //作成者Nameの取得
+    public String getAutherName() {
+        return autherUserName;
     }
 
-    public String getPassword() {
-        return password;
+    //作成者Noの取得
+    public String getAutherNo() {
+        return autherUserNo;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+
+    //全ユーザー名の取得
+    public ArrayList<String> getUserName() {
+        return userNameList;
+    }
+
+    //全ユーザー名の設定
+    public void setUserName(String userName) {
+        userNameList.add(userName);
+    }
+
+    //全会員番号の取得
+    public ArrayList<String> getUserNo() {
+        return userNumList;
+    }
+
+    //全会員番号の設定
+    public void setUserNo(String userNo) {
+        userNumList.add(userNo);
     }
 
     public String getErrorMessage() {
@@ -43,29 +92,4 @@ public class MakeGroupBean {
         this.errorMessage = errorMessage;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserNo() {
-        return userNo;
-    }
-
-    public void setUserNo(String userNo) {
-        this.userNo = userNo;
-    }
-
-    public String getGroupNo() {
-        return groupNo;
-    }
-
-    public void setGroupNo(String groupNo) {
-        this.groupNo = groupNo;
-    }
-
 }
-
