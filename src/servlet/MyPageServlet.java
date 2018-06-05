2dc30d8ab2cb579782f37a501b08cdffc947195a
yamagishi-toko <yamagishi-toko@KMS2013-78.kms.local>
2018-06-05 17:02:04 +0900

    他プロフィール確認機能 & プロフィール編集機能

--- src/servlet/MyPageServlet.java
+++ src/servlet/MyPageServlet.java
@@ -1,8 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -33,14 +31,8 @@ public class MyPageServlet extends HttpServlet {
         MyPageModel model = new MyPageModel();
         String direction = "/WEB-INF/jsp/myPage.jsp";
 
-        // パラメータの取得
-//        String userNo = (String) req.getParameter("userNo");
-//        String userName = (String) req.getParameter("userName");
-//        String myPageText = (String) req.getParameter("myPageText");
-
         //セッション情報の取得
         HttpSession session = req.getSession();
-        //session.getAttribute(userNo);
         bean1 = (SessionBean) session.getAttribute("session");
         bean2.setUserNo(bean1.getUserNo());
 
@@ -81,47 +73,15 @@ public class MyPageServlet extends HttpServlet {
         String userNo = (String) req.getParameter("userNo");
         String userName = (String) req.getParameter("userName");
         String myPageText = (String) req.getParameter("myPageText");
-        //Pattern p1 = Pattern.compile("^[0-9a-zA-Z]*$");    //半角英数or空白
-        Pattern p = Pattern.compile("^[^-~｡-ﾟ]*$");    //全角
-        Matcher mUserName = p.matcher(userName);    //←エラーここ
-        Matcher mMyPageText = p.matcher(myPageText);
 
         //"プロフィールを更新"のリクエスト送信後、入力値チェック
-        if (userName.length() > 30 ) {
-            if(myPageText.length() > 100) {
-                if(!mUserName.find()) {
-                    if(!mMyPageText.find()) {
-                        bean2.setErrorMessage("表示名は30文字以下、自己紹介は100文字以下の全角で入力してください。");
-                    }
-                }
-                bean2.setErrorMessage("表示名は30文字以下で入力してください。");
-            }
-
-        }else if (myPageText.length() > 100) {
-            bean2.setErrorMessage("自己紹介は100文字以下で入力してください。");
-        } else if (!mUserName.find()) {
-            bean2.setErrorMessage("入力された値は正しくありません");
-        } else if (!mMyPageText.find()) {
-            bean2.setErrorMessage("入力された値は正しくありません");
+        if (userName.length() > 30 || myPageText.length() > 100) {
+        	bean2.setErrorMessage("表示名は30文字以下、自己紹介文は100文字以下で入力してください。");
         } else {
             bean2.setUserName(userName);
             bean2.setMyPageText(myPageText);
             bean2.setErrorMessage("");
         }
-
-        // 認証処理
-//        try {
-//            bean2 = model.authentication(bean2);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        // プロフィール更新処理
-//      try {
-//          bean2 = model.authentication2(bean2);
-//      } catch (Exception e) {
-//          e.printStackTrace();
-//      }
 
         // 更新処理が成功した場合セッションに情報をセット
         bean2.setUserNo(userNo);
@@ -145,4 +105,5 @@ public class MyPageServlet extends HttpServlet {
         req.getRequestDispatcher(direction).forward(req, res);
 
     }
+
 }
